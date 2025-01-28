94b7a51494924e1baec6f322ddba2638d4f03764
EriickChavez <10erickchavez10@gmail.com>
2025-01-28 06:40:09 -0600

    remove dist

--- dist/db/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//reads env vars and decides which database to use
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = process.env.DBTYPE === "postgres"
-    ? require("postgres")
-    : require("mysql2");
