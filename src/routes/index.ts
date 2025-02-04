f4110e07900249fc5e1e42b2d8fc22794dea4c00
EriickChavez <10erickchavez10@gmail.com>
2025-02-04 17:20:09 -0600

    license

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes";
 import questionRoutes from "./questionRoutes";
+import categoryRoutes from "./categoryRoutes";
 
 export default () => {
   const app = Router();
 
   authRoutes(app);
   questionRoutes(app);
+  categoryRoutes(app);
 
   return app;
 };
