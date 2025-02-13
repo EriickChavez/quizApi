37b3674004097ae94e97032818e10ef41baa9120
EriickChavez <10erickchavez10@gmail.com>
2025-02-13 01:55:45 -0600

    fix: update deploy script to use yarn and simplify server startup logging

--- src/server.ts
+++ src/server.ts
@@ -6,27 +6,11 @@ import os from "os";
 
 const PORT = ENV.PORT;
 
-const getServerIp = () => {
-  const interfaces = os.networkInterfaces();
-  let ipAddress = "127.0.0.1"; // Default to localhost
-  for (const interfaceName in interfaces) {
-    // @ts-ignore
-    for (const iface of interfaces[interfaceName]) {
-      if (iface.family === "IPv4" && !iface.internal) {
-        ipAddress = iface.address;
-        break;
-      }
-    }
-  }
-  return ipAddress;
-};
-
 const startServer = async () => {
   // await connectMySQL();
   await connectMongoDB();
-  const ip = getServerIp();
   app.listen(PORT, () => {
-    logger.info(`Server is running at http://${ip}:${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
   });
 };
 
