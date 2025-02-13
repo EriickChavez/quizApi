34211ddcf2cee575b8af1aa68a27ff5d67e33d00
EriickChavez <10erickchavez10@gmail.com>
2025-02-13 01:52:31 -0600

    feat: retrieve and log server IP address on startup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,32 @@
 import app from "./app";
 import { connectMongoDB } from "./config/db";
 import { ENV } from "./config/ENV";
-// import { connectMySQL } from "./config/db";
 import logger from "./middleware/logger";
+import os from "os";
 
 const PORT = ENV.PORT;
+
+const getServerIp = () => {
+  const interfaces = os.networkInterfaces();
+  let ipAddress = "127.0.0.1"; // Default to localhost
+  for (const interfaceName in interfaces) {
+    // @ts-ignore
+    for (const iface of interfaces[interfaceName]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        ipAddress = iface.address;
+        break;
+      }
+    }
+  }
+  return ipAddress;
+};
 
 const startServer = async () => {
   // await connectMySQL();
   await connectMongoDB();
+  const ip = getServerIp();
   app.listen(PORT, () => {
-    logger.info(`Server is running on port http://localhost:${PORT}`);
+    logger.info(`Server is running at http://${ip}:${PORT}`);
   });
 };
 
