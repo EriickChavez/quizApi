ad8ca09b3d63769d416793f007418070f08c057a
EriickChavez <10erickchavez10@gmail.com>
2025-06-14 17:54:35 -0600

    fix: correct import path casing for AnswerHistoryMongoRepository

--- src/services/instances/answerHistoryServiceInstance.ts
+++ src/services/instances/answerHistoryServiceInstance.ts
@@ -1,5 +1,4 @@
-
-import { AnswerHistoryMongoRepository } from "../../repositories/mongodb/AnswerHistoryMongoRepository";
+import { AnswerHistoryMongoRepository } from "../../repositories/mongodb/answerHistoryMongoRepository";
 import { AnswerHistoryService } from "../answerHistory/AnswerHistoryService";
 
 export const answerHistoryServiceInstance = new AnswerHistoryService(new AnswerHistoryMongoRepository());
